280dff1d823efbefe111fd759affc01a6ded1922
Ednezer de Godoi Rezende <erezende@brq.com>
2022-04-12 20:00:34 -0300

    ByCoders - Inclusao de projeto frontEnd - Parte inicial com navegacao entre paginas e envio de arquivo - 1 parte

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: 'pages',
+    loadChildren: () => import('./pages/pages.module').then((pages) => pages.PagesModule),
+  },
+  {path:'', redirectTo:'pages',pathMatch: 'full'},
+  {path: '**', redirectTo: 'pages'}
 
-const routes: Routes = [];
+];
+
+const config: ExtraOptions ={
+  useHash: false,
+  onSameUrlNavigation: 'reload'
+}
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, config)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
